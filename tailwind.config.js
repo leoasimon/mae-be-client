71623cbb11a7e742bfc2c43c06ae3e66b5ff7dc0
lygsim <leo@lsimon.org>
2026-03-23 19:52:29 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,njk}"],
+  theme: {
+    colors: {
+      lightgray: '#F8F7F2',
+      blue: '#052E68',
+      white: '#FFFFFF'
+    },
+    extend: {},
+  },
+  plugins: [],
+};
