f98b07102e83230d3f937424f4c1860bcea9f2c2
lygsim <leo@lsimon.org>
2026-03-25 20:37:13 +0100

    mobile menu

--- src/static/menu.js
+++ src/static/menu.js
@@ -3,14 +3,16 @@ function toggleMenu() {
 
     if (overlay.classList.contains("hidden")) {
         overlay.classList.remove('hidden');
+        document.getElementsByTagName("body")[0].classList.add("overflow-hidden");
     } else {
         overlay.classList.add('hidden');
+        document.getElementsByTagName("body")[0].classList.remove("overflow-hidden");
     }
 }
 
 window.onload = function() {
     const menuBtn = document.getElementById("menu-btn");
-    const closeBtn = document.getElementById("close-menu-btn");
+    const closeBtn = document.getElementById("close-btn");
 
     menuBtn.addEventListener('click', toggleMenu);
     closeBtn.addEventListener('click', toggleMenu);
